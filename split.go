7f3191e4a9fb99718aa4d5934b0778e30cc1a84d
atma <atma@ubuntu.(none)>
2013-11-16 14:57:31 +1030

    Add main and split

---
+++ split.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"code.google.com/p/biogo/alphabet"
+	"code.google.com/p/biogo/io/seqio"
+	"code.google.com/p/biogo/io/seqio/fasta"
+	"code.google.com/p/biogo/seq/linear"
+
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func split(inputFileName string) {
+	// open inputFile in read-only mode
+	inputFile, inputError := os.Open(inputFileName)
+	if inputError != nil {
+		fmt.Println("An error occurred opening the input file.")
+		return
+	}
+	defer inputFile.Close()
+
+	sc := seqio.NewScanner(fasta.NewReader(inputFile, linear.NewSeq("", nil, alphabet.DNA)))
+	for sc.Next() {
+		s := sc.Seq().(*linear.Seq)
+
+		// open outputFile in write-only mode
+		outputFile, outputError := os.Create(s.ID + ".fa")
+		if outputError != nil {
+			fmt.Println("Could not create file " + s.ID + ".fa")
+			return
+		}
+		defer outputFile.Close()
+
+		outputWriter := bufio.NewWriter(outputFile)
+
+		// write header
+		outputWriter.WriteString(">" + s.ID + "\n")
+		// write FASTA sequence
+		outputWriter.WriteString(s.String() + "\n")
+
+		// write the buffer completely to the file
+		outputWriter.Flush()
+	}
+	if sc.Error() != nil {
+		panic(sc.Error())
+	}
+
+}
