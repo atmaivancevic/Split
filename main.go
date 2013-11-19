12b5779b637ee231be4ceee3ab036838540f69da
Atma <atma.ivancevic@adelaide.edu.au>
2013-11-19 14:59:08 +1030

    test on Alpaca genome

--- main.go
+++ main.go
@@ -1,5 +1,23 @@
 package main
 
+import (
+	"os"
+	"fmt"
+)
+
+const genomeDir = "scratch/atmaGenomes/"
+
+func splitGenome(genomeName, extension, identifier string) {
+
+	os.Chdir(genomeDir + genomeName)
+
+	inputFileName := identifier + "." + extension
+
+	split(inputFileName)
+	fmt.Println("Processed: " + genomeName + "/" identifier)
+
+}
+
 func main() {
-	split("genome.fa")
+	splitGenome("Alpaca", "fa", "vicPac2")
 }
