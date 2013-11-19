995132fd14ea8a5df2b20c3bfb9106389c5c9153
Atma <atma.ivancevic@adelaide.edu.au>
2013-11-19 15:16:38 +1030

    add if statement

--- main.go
+++ main.go
@@ -13,8 +13,13 @@ func splitGenome(genomeName, extension, identifier string) {
 
 	inputFileName := identifier + "_unmasked." + extension
 
-	split(inputFileName)
-	fmt.Println("Processed: " + genomeName + "/" + identifier + "_unmasked")
+	// check if the genome unmasked file exists - if so, split it
+	if _, err := os.Stat(inputFileName); err == nil {
+		split(inputFileName)
+		fmt.Println("Processed: " + genomeName + "/" + identifier + "_unmasked")
+	} else {
+		fmt.Println("File " genomeName + "/" + identifier + "_unmasked does not exist")
+	}
 
 }
 
